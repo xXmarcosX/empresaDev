f2861ba993e81580733e26bcbd1a13460f82409e
Thurtux <arthursantoslopes91@gmail.com>
2024-06-26 10:49:38 -0300

    Adicionando contato e alterado links

---
+++ contato.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    const mailtoLink = `mailto:contato@cyrax.com?subject=Contato de ${name}&body=${encodeURIComponent(message)}%0D%0A%0D%0ADe: ${name}%0D%0AEmail: ${email}`;
+    
+    window.location.href = mailtoLink;
+
+    setTimeout(function() {
+        window.location.href = 'index.html';  // Redireciona para a página inicial após 3 segundos
+    }, 3000);
+});
